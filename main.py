19cf0e91b647223d7814078ea70bb89113f2f9ae
dawtom97 <dawtom97@gmail.com>
2026-04-20 18:13:45 +0200

    save to mysql function

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from services.openweather_api import get_weather
 from services.excel_files import save_to_excel, read_excel
 from services.dashboard import render_dashboard
+from services.mysql_db import create_db_and_table, save_to_mysql
 import time
+
+create_db_and_table()
 
 while True:
     weather = get_weather()
     save_to_excel([weather])
+    save_to_mysql(weather)
     print("Pobieram i zapisuję dane")
     time.sleep(5)
 
